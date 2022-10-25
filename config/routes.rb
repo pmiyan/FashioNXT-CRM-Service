5ae6313697d83a021c045d70e959afe5aa8f8dd9
varshanipreddy <varshanipreddy@gmail.com>
2022-10-24 22:20:58 -0500

    db connect

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'users#index'
+  post '/sessions' => 'sessions#create'
+  post '/users' => 'users#create'
+  get '/dashboard' => 'crms#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/home' => 'home#index'
 end
