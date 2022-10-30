cfa1830f6dc992e523bd332778464e5988695c6d
anudeepikavzf03 <anudeepikavzf03@tamu.edu>
2022-10-30 17:01:26 -0500

    itr 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   post '/sessions' => 'sessions#create'
   post '/users' => 'users#create'
   get '/dashboard' => 'crms#index'
-
+  get '/admin' => 'admin#index'
+get 'logs' => 'logs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/home' => 'home#index'
 end
